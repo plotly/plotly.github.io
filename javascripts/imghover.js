0d5a0f66e45ff9d1ad0d5b4a9ee7ae1f168c0e89
NicoleG <Nicolemgrondin@gmail.com>
2015-05-01 10:59:15 -0400

    Create imghover.js

---
+++ javascripts/imghover.js
@@ -0,0 +1,36 @@
+function movingInMat() {
+		var hidethisimg=document.getElementById('matlabImg');
+		var showthisimg=document.getElementById('matlabText');
+		hidethisimg.style.display = "none";
+		showthisimg.style.display = "";
+}
+function movingOutMat() {	
+		var hidethisimg=document.getElementById('matlabText');
+		var showthisimg=document.getElementById('matlabImg');
+		hidethisimg.style.display = "none";
+		showthisimg.style.display = "";
+}	
+function movingInPy() {
+		var hidethisimg=document.getElementById('pyImg');
+		var showthisimg=document.getElementById('pyText');
+		hidethisimg.style.display = "none";
+		showthisimg.style.display = "";
+}
+function movingOutPy() {	
+		var hidethisimg=document.getElementById('pyText');
+		var showthisimg=document.getElementById('pyImg');
+		hidethisimg.style.display = "none";
+		showthisimg.style.display = "";
+}	
+function movingInR() {
+		var hidethisimg=document.getElementById('rImg');
+		var showthisimg=document.getElementById('rText');
+		hidethisimg.style.display = "none";
+		showthisimg.style.display = "";
+}
+function movingOutR() {	
+		var hidethisimg=document.getElementById('rText');
+		var showthisimg=document.getElementById('rImg');
+		hidethisimg.style.display = "none";
+		showthisimg.style.display = "";
+}	
