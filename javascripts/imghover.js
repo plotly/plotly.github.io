232a4dd37a222535998dd5e5ce4631fc65788315
Nicole Grondin <krachum@hotmail.com>
2015-05-11 09:59:50 -0400

    JS img hover function update

--- javascripts/imghover.js
+++ javascripts/imghover.js
@@ -1,73 +1,8 @@
-function movingInMat() {
-		var hidethisimg=document.getElementById('matlabImg');
-		var showthisimg=document.getElementById('matlabText');
-		hidethisimg.style.opacity = "0.2";
-		showthisimg.style.display = "";
+function movingInImgOp(img, text) {
+		img.style.opacity = "0.2";
+		text.style.display = "";
 }
-function movingOutMat() {	
-		var hidethisimg=document.getElementById('matlabText');
-		var showthisimg=document.getElementById('matlabImg');
-		hidethisimg.style.display = "none";
-		showthisimg.style.opacity = "1";
-}	
-function movingInPy() {
-		var hidethisimg=document.getElementById('pyImg');
-		var showthisimg=document.getElementById('pyText');
-		hidethisimg.style.opacity = "0.2";
-		showthisimg.style.display = "";
-}
-function movingOutPy() {	
-		var hidethisimg=document.getElementById('pyText');
-		var showthisimg=document.getElementById('pyImg');
-		hidethisimg.style.display = "none";
-		showthisimg.style.opacity = "1";
-}	
-function movingInR() {
-		var hidethisimg=document.getElementById('rImg');
-		var showthisimg=document.getElementById('rText');
-		hidethisimg.style.opacity = "0.2";
-		showthisimg.style.display = "";
-}
-function movingOutR() {	
-		var hidethisimg=document.getElementById('rText');
-		var showthisimg=document.getElementById('rImg');
-		hidethisimg.style.display = "none";
-		showthisimg.style.opacity = "1";
-}	
-
-function movingInThumbA() {
-		var hidethisimg=document.getElementById('thumimgA');
-		var showthisimg=document.getElementById('thumtextA');
-		hidethisimg.style.opacity = "0.2";
-		showthisimg.style.display = "";
-}
-function movingOutThumbA() {	
-		var hidethisimg=document.getElementById('thumtextA');
-		var showthisimg=document.getElementById('thumimgA');
-		hidethisimg.style.display = "none";
-		showthisimg.style.opacity = "1";
-}
-function movingInThumbB() {
-		var hidethisimg=document.getElementById('thumimgB');
-		var showthisimg=document.getElementById('thumtextB');
-		hidethisimg.style.opacity = "0.2";
-		showthisimg.style.display = "";
-}
-function movingOutThumbB() {	
-		var hidethisimg=document.getElementById('thumtextB');
-		var showthisimg=document.getElementById('thumimgB');
-		hidethisimg.style.display = "none";
-		showthisimg.style.opacity = "1";
-}
-function movingInThumbC() {
-		var hidethisimg=document.getElementById('thumimgC');
-		var showthisimg=document.getElementById('thumtextC');
-		hidethisimg.style.opacity = "0.2";
-		showthisimg.style.display = "";
-}
-function movingOutThumbC() {	
-		var hidethisimg=document.getElementById('thumtextC');
-		var showthisimg=document.getElementById('thumimgC');
-		hidethisimg.style.display = "none";
-		showthisimg.style.opacity = "1";
+function movingOutImgOp(img, text) {	
+		text.style.display = "none";
+		img.style.opacity = "1";
 }
